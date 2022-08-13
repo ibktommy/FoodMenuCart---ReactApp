5e612fd405bd3382b5f894e0b4532981ce47c4e1
ibktommy <atomquartz@gmail.com>
2022-08-13 20:14:53 +0100

    Create Card UI component that is basically a styled-component

--- src/components/MealsContent/MealsList/MealsList.js
+++ src/components/MealsContent/MealsList/MealsList.js
@@ -1,19 +1,22 @@
 import React from 'react'
 import classes from './MealsList.module.css'
 import mealsData from '../MealsData'
+import Card from '../../UI/Card/Card'
 
 const MealsList = () => {
   return (
     <section className={classes.meals}>
-      <ul>
-        {
-          mealsData.map((meal) => {
-            return (
-              <h1>{meal.name}</h1>
-            )
-          })
-        }
-      </ul>
+      <Card>
+        <ul>
+          {
+            mealsData.map((meal) => {
+              return (
+                <h1>{meal.name}</h1>
+              )
+            })
+          }
+        </ul>
+      </Card>
     </section>
   )
 }
