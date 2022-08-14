f4439fc86ed63b64ce33622e07a435065b621f9d
ibktommy <atomquartz@gmail.com>
2022-08-14 13:59:28 +0100

    Create Functions that will be used in the CartContextProvider value-object

--- src/contextStore/CartProvider.js
+++ src/contextStore/CartProvider.js
@@ -2,10 +2,17 @@ import React from 'react'
 import CartContext from './cart-context'
 
 const CartProvider = ({ children }) => {
+  // Create Functions that will be added to the Context Values
+  const addItemToCartHandler = (item) => {}
+  const removeItemFromCartHandler = () => {}
+
+
   // Creating the Context Value Object
   const cartContext = {
     items: [],
     totalAmount: 0,
+    addItemToCart: addItemToCartHandler,
+    removeItemFromCart: removeItemFromCartHandler
   }
 
   return (
