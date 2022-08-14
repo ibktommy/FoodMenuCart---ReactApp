1eb0b6142d000799ef94b356317ad8d028d7feed
ibktommy <atomquartz@gmail.com>
2022-08-14 12:49:29 +0100

    Create The Cart Component and structure it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header/Header';
 import MealsContent from './components/MealsContent/MealsContent';
 
 function App() {
   return (
     <>
+      <Cart />
       <Header />
       <MealsContent />
     </>
---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import classes from './Cart.module.css'
+
+const Cart = () => {
+  // Setting Dummy Data for Cart
+  const dummyCartData = [
+    {
+      id: 'c1',
+      name: 'Sushi',
+      amount: 2,
+      price: 12.99,
+    }
+  ]
+
+
+  return (
+    <>
+      <ul className={classes['cart-items']}>
+        {
+          dummyCartData.map((cartItem) => {
+            return (
+              <li key={cartItem.id}>
+                {cartItem.name}
+              </li>
+            )
+          })
+        }
+      </ul>
+
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>35.62</span>
+      </div>
+
+      <div className={classes.actions}>
+        <button className={classes['button--alt']}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </>
+  )
+}
+
+export default Cart
