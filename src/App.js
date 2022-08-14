8f566870e7d53dc1fe5362d43fde84b209a9a3e6
ibktommy <atomquartz@gmail.com>
2022-08-14 13:15:46 +0100

    Create state on APPjs which functions are set to monitor and modify based on set actions/events

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header/Header';
 import MealsContent from './components/MealsContent/MealsContent';
 
 function App() {
+  // Setting APp State
+  const [showCart, setShowCart] = useState(false)
+
+  // Function Handler to Display/Hide Cart
+  const showCartHandler = () => {
+    setShowCart(true)
+  }
+  const hideCartHandler = () => {
+    setShowCart(false)
+  }
+
   return (
     <>
-      <Cart />
+      {showCart && <Cart />}
       <Header />
-      <MealsContent />
+      <main>
+        <MealsContent />
+      </main>
     </>
   );
 }
