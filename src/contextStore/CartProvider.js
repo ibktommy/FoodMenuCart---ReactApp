b932f5563418c3ca93360d94987c64b274105b81
ibktommy <atomquartz@gmail.com>
2022-08-15 12:39:59 +0100

    Create Separate file to handle the cartReducerFunction and then replace the'return defaultState' with a 'throw Error' in the function

--- src/contextStore/CartProvider.js
+++ src/contextStore/CartProvider.js
@@ -1,40 +1,12 @@
 import React, { useReducer } from 'react'
 import CartContext from './cart-context'
-// import { cartReducerFunc } from '../components/Cart/CartReducer/cartReducerFunc'
+import { cartReducerFunc } from '../components/Cart/CartReducer/cartReducerFunc'
 
 // CART REDUCER
 // Set Default/Initial State
 const defaultCartState = {
   items: [],
   totalAmount: 0
-}
-
-const cartReducerFunc = (state, action) => {
-  if (action.type === 'ADD') {
-    const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount
-
-    const existingCartItemIndex = state.items.findIndex((item) => item.id === action.item.id)
-    const existingCartItem = state.items[existingCartItemIndex]
-    let updatedItems;
-
-    if (existingCartItem) {
-      const updatedItem = {
-        ...existingCartItem,
-        amount: existingCartItem.amount + action.item.amount
-      }
-      updatedItems = [...state.items]
-      updatedItems[existingCartItemIndex] = updatedItem
-    } else {
-      updatedItems = state.items.concat(action.item)
-    }
-
-    return {
-      items: updatedItems,
-      totalAmount: updatedTotalAmount
-    }
-  }
-
-  return defaultCartState
 }
 
 
