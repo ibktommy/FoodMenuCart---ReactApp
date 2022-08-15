fba4f4c5b6a4b3e677babf1ffd7df76ad7487697
ibktommy <atomquartz@gmail.com>
2022-08-15 13:27:02 +0100

    // Using UseEffect to Effect the Cart Button Animation anytime the cartItems is changed/re-rendered

--- src/components/Layout/HeaderCartBtn/HeaderCartBtn.js
+++ src/components/Layout/HeaderCartBtn/HeaderCartBtn.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import CartContext from '../../../contextStore/cart-context'
 import CartIcon from '../../Cart/CartIcon'
 import classes from './HeaderCartBtn.module.css'
@@ -6,13 +6,39 @@ import classes from './HeaderCartBtn.module.css'
 const HeaderCartBtn = (props) => {
   // Using The Context
    const cartContext =  useContext(CartContext)
+  //  Setting State to monitor the Cart Button Animation
+  const [cartButtonBump, setCartButtonBump] = useState(false)
 
    const numberOfCartItems = cartContext.items.reduce((currentNumber, item) => {
     return currentNumber + item.amount
    }, 0)
 
+  //  Pulling Cart Items out from the Cart Context
+  const { items } = cartContext
+
+  // Setting Dynamic Class for the cart Button
+  const cartButton = `${classes.button} ${cartButtonBump ? classes.bump : ''}`
+
+  // Using UseEffect to Effect the Cart Button Animation anytime the cartItems is changed/re-rendered
+  useEffect(() => {
+    if (items.length === 0) {
+      return
+    }
+
+    setCartButtonBump(true)
+
+    const timer = setTimeout(() => {
+      setCartButtonBump(false)
+    }, 300)
+
+    // Clear setTimeout
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [items])
+
   return (
-    <button className={classes.button} onClick={props.openCart}>
+    <button className={cartButton} onClick={props.openCart}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
